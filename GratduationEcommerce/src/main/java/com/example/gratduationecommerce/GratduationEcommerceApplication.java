a2b489aabd169f50eb0d271f07befbec1ba55349
dunghcmut <letrananhdung234@gmail.com>
2026-03-09 21:22:00 +0700

    init project

---
+++ GratduationEcommerce/src/main/java/com/example/gratduationecommerce/GratduationEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gratduationecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GratduationEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GratduationEcommerceApplication.class, args);
+    }
+
+}
---
+++ GratduationEcommerce/src/test/java/com/example/gratduationecommerce/GratduationEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gratduationecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GratduationEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
